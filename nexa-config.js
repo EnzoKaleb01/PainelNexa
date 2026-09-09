d13f105942a8881c53ee6e2f62071f0f39ba5fef
Enzo Kaleb <Enzokaleb01@gmail.com>
2026-09-09 16:37:47 -0300

    Update API endpoint in nexa-config.js

--- nexa-config.js
+++ nexa-config.js
@@ -11,5 +11,5 @@ window.NEXA_CONFIG = {
      É isso que faz os dois conversarem.
      Termina sempre com /exec
      --------------------------------------------------------- */
-  api: 'COLE_AQUI_O_ENDERECO_DO_APPS_SCRIPT'
+  api: 'nx-cm24ec5qxl37vhi7oay5bezpe875'
 };
